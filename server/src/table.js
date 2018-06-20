fe23fd6e56bd1cdcf3afd6d6844acc1d2168e25d
Josh Dronsfield <josh.dronsfield@redant.com>
2018-06-20 19:11:37 +0100

    still doing the table resolver WIP

--- server/src/table.js
+++ server/src/table.js
@@ -1,6 +1,6 @@
 import { fromEvent, FunctionEvent } from 'graphcool-lib'
 import { GraphQLClient } from 'graphql-request'
-import { path } from 'ramda'
+import { get } from 'lodash/fp'
 
 async function getAllMatches(api) {
   const query = `
@@ -31,6 +31,18 @@ async function getAllMatches(api) {
   return api.request(query)
 }
 
+async function getAllUsers(api) {
+  const query = `
+  query {
+    allUsers {
+      id
+      name
+    }
+  }`
+  
+  return api.request(query)
+}
+
 const playerKeys = [
   'homePlayer1',
   'homePlayer2',
@@ -40,12 +52,12 @@ const playerKeys = [
 
 function getPermutationIdFromMatch (match) {
   const homePlayerIds = [
-    path('homePlayer1.id')(match),
-    path('homePlayer2.id')(match)
+    get('homePlayer1.id')(match),
+    get('homePlayer2.id')(match)
   ]
   const awayPlayerIds = [
-    path('awayPlayer1.id')(match),
-    path('awayPlayer2.id')(match)
+    get('awayPlayer1.id')(match),
+    get('awayPlayer2.id')(match)
   ]
   const homePlayersKey = homePlayerIds.sort().join('+')
   const awayPlayersKey = awayPlayerIds.sort().join('+')
@@ -77,7 +89,7 @@ function shapePermutationForUser (shapedMatchesForUser = []) {
     }
   }, {})
 
-  const permutationId = path('0.permutationId')(shapedMatchesForUser)
+  const permutationId = get('0.permutationId')(shapedMatchesForUser)
   const matches = shapedMatchesForUser.length
   const averageGoalsFor = goalsFor / matches
   const averageGoalsAgainst = goalsAgainst / matches
@@ -108,7 +120,7 @@ function getTableFromMatches (matches) {
 
   matches.forEach(match => {
     playerKeys.forEach(key => {
-      const userId = path(`${key}.id`)(match)
+      const userId = get(`${key}.id`)(match)
       const shapedMatch = shapeMatchForUser(match, key)
       usersMatches[userId] = (
         usersMatches[userId]
@@ -116,6 +128,14 @@ function getTableFromMatches (matches) {
         : [shapedMatch]
       )
     })
+  })
+
+  const table = {}
+
+  Object.entries(usersMatches).forEach(([userId, matches]) => {
+    const permutationsObj = matches.reduce((acc, match) => {
+      
+    }, {})
   })
 
   return { msg: usersMatches }
@@ -129,12 +149,13 @@ export default async (event) => {
     const api = graphcool.api('simple/v1')
 
     const { allMatches } = await getAllMatches(api)
+    const { allUsers } = await getAllUsers(api)
 
-    const table = getTableFromMatches(allMatches)
+    const table = getTableFromMatches(allMatches, allUsers)
 
     return {
       data: {
-        rows: allMatches,
+        rows: {},
         info: table
       }
     }
